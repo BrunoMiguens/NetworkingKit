ed0ce26862c833a23959ba77ef7f3a526f42e9d1
Bruno Miguens <brunomiguens03@hotmail.com>
2018-03-26 21:54:03 +0100

    Fixed some files with the wrong access level

--- NetworkingKit/Source/Components/NetworkingResult.swift
+++ NetworkingKit/Source/Components/NetworkingResult.swift
@@ -12,7 +12,6 @@ public typealias NKHandler = (_ result: NKResult<Any>) -> Void
 public typealias NKArrayHandler = (_ result: NKResult<NKArray>) -> Void
 public typealias NKJsonHandler = (_ result: NKResult<NKDictionary>) -> Void
 
-// swiftlint:disable shorthand_operator
 
 /// This enum represents the kind of types that a result handler could return.
 public enum NKResult<T> {
@@ -23,36 +22,40 @@ public enum NKResult<T> {
     /// If something goes wrong this '.Failure' will be used, will return 'NKError' enum to know what happens.
     case Failure(NKError)
 
-    /// This property should return the value inside of the success case, if applicable.
-    var value: T? {
-        switch self {
-
-        case .Success(let obj): return obj
-        default: return nil
-
-        }
-    }
-
-    /// This property should return the value of type `NKError` inside of the error case, in case of nonexistent value should return a `.unknown` value.
-    var error: NKError {
-        switch self {
-
-        case .Failure(let error): return error
-        default: return .unknown
-
-        }
-    }
-
-    /// This property should return a boolean value representing if is a success case or not.
-    var isSuccess: Bool {
-        switch self {
-
-        case .Success: return true
-        default: return false
-
-        }
-    }
-
 }
 
-// swiftlint:enable shorthand_operator
+extension NKResult {
+    
+    // MARK: Helpers
+    
+    /// This property should return the value inside of the success case, if applicable.
+    public var value: T? {
+        switch self {
+            
+        case .Success(let obj): return obj
+        default: return nil
+            
+        }
+    }
+    
+    /// This property should return the value of type `NKError` inside of the error case, in case of nonexistent value should return a `.unknown` value.
+    public var error: NKError {
+        switch self {
+            
+        case .Failure(let error): return error
+        default: return .unknown
+            
+        }
+    }
+    
+    /// This property should return a boolean value representing if is a success case or not.
+    public var isSuccess: Bool {
+        switch self {
+            
+        case .Success: return true
+        default: return false
+            
+        }
+    }
+    
+}
